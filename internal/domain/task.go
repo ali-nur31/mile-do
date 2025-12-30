10ec0386e90a2507b07245f352ac5810d25b1fe0
Ali-Nur <alinur.sagynbaev1@gmail.com>
2025-12-30 14:40:57 +0600

    added service for tasks

---
+++ internal/domain/task.go
@@ -0,0 +1,43 @@
+package domain
+
+import "time"
+
+type GetTasksByPeriodInput struct {
+	UserID     int32
+	AfterDate  time.Time
+	BeforeDate time.Time
+}
+
+type CreateTaskInput struct {
+	UserID          int32
+	GoalID          int32
+	Title           string
+	ScheduledDate   time.Time
+	ScheduledTime   time.Time
+	DurationMinutes int16
+}
+
+type UpdateTask struct {
+	ID              int64
+	UserID          int32
+	GoalID          int32
+	Title           string
+	IsDone          bool
+	ScheduledDate   time.Time
+	ScheduledTime   time.Time
+	DurationMinutes int16
+	RescheduleCount int32
+}
+
+type TaskOutput struct {
+	ID              int64
+	UserID          int32
+	GoalID          int32
+	Title           string
+	IsDone          bool
+	ScheduledDate   time.Time
+	ScheduledTime   time.Time
+	DurationMinutes int
+	RescheduleCount int32
+	CreatedAt       time.Time
+}
---
+++ internal/service/task_service.go
@@ -0,0 +1,259 @@
+package service
+
+import (
+	"context"
+	"time"
+
+	repo "github.com/ali-nur31/mile-do/internal/db"
+	"github.com/ali-nur31/mile-do/internal/domain"
+	"github.com/jackc/pgx/v5/pgtype"
+)
+
+type TaskService interface {
+	ListTasksByGoalID(ctx context.Context, userId int32, goalId int32) (*[]domain.TaskOutput, error)
+	ListInboxTasks(ctx context.Context, userId int32) (*[]domain.TaskOutput, error)
+	ListTasksByPeriod(ctx context.Context, period domain.GetTasksByPeriodInput) (*[]domain.TaskOutput, error)
+	ListTasks(ctx context.Context, userId int32) (*[]domain.TaskOutput, error)
+	GetTaskByID(ctx context.Context, id int64, userId int32) (*domain.TaskOutput, error)
+	CreateTask(ctx context.Context, input domain.CreateTaskInput) (*domain.TaskOutput, error)
+	UpdateTask(ctx context.Context, dbTask domain.TaskOutput, updatingTask domain.UpdateTask) (*domain.UpdateTask, error)
+	AnalyzeForToday(ctx context.Context, userId int32) (repo.CountCompletedTasksForTodayRow, error)
+	DeleteTaskByID(ctx context.Context, id int64, userId int32) error
+}
+
+type taskService struct {
+	repo repo.Querier
+}
+
+func NewTaskService(repo repo.Querier) TaskService {
+	return &taskService{
+		repo: repo,
+	}
+}
+
+func (s *taskService) ListTasksByGoalID(ctx context.Context, userId int32, goalId int32) (*[]domain.TaskOutput, error) {
+	var output []domain.TaskOutput
+
+	tasks, err := s.repo.ListTasksByGoalID(ctx, repo.ListTasksByGoalIDParams{
+		UserID: userId,
+		GoalID: goalId,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	for _, task := range tasks {
+		output = append(output, domain.TaskOutput{
+			ID:              task.ID,
+			UserID:          task.UserID,
+			GoalID:          task.GoalID,
+			Title:           task.Title,
+			IsDone:          task.IsDone,
+			ScheduledDate:   task.ScheduledDate.Time,
+			ScheduledTime:   convertMcSecToTime(task.ScheduledTime.Microseconds),
+			RescheduleCount: task.RescheduleCount,
+			CreatedAt:       task.CreatedAt.Time,
+		})
+	}
+
+	return &output, nil
+}
+
+func (s *taskService) ListInboxTasks(ctx context.Context, userId int32) (*[]domain.TaskOutput, error) {
+	var output []domain.TaskOutput
+
+	tasks, err := s.repo.ListInboxTasks(ctx, userId)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, task := range tasks {
+		output = append(output, domain.TaskOutput{
+			ID:              task.ID,
+			UserID:          task.UserID,
+			GoalID:          task.GoalID,
+			Title:           task.Title,
+			IsDone:          task.IsDone,
+			ScheduledDate:   task.ScheduledDate.Time,
+			ScheduledTime:   convertMcSecToTime(task.ScheduledTime.Microseconds),
+			RescheduleCount: task.RescheduleCount,
+			CreatedAt:       task.CreatedAt.Time,
+		})
+	}
+
+	return &output, nil
+}
+
+func (s *taskService) ListTasksByPeriod(ctx context.Context, period domain.GetTasksByPeriodInput) (*[]domain.TaskOutput, error) {
+	var output []domain.TaskOutput
+
+	tasks, err := s.repo.ListTasksByDateRange(ctx, repo.ListTasksByDateRangeParams{
+		UserID: period.UserID,
+		ScheduledDate: pgtype.Date{
+			Time:  period.AfterDate,
+			Valid: true,
+		},
+		ScheduledDate_2: pgtype.Date{
+			Time:  period.BeforeDate,
+			Valid: true,
+		},
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	for _, task := range tasks {
+		output = append(output, domain.TaskOutput{
+			ID:              task.ID,
+			UserID:          task.UserID,
+			GoalID:          task.GoalID,
+			Title:           task.Title,
+			IsDone:          task.IsDone,
+			ScheduledDate:   task.ScheduledDate.Time,
+			ScheduledTime:   convertMcSecToTime(task.ScheduledTime.Microseconds),
+			RescheduleCount: task.RescheduleCount,
+			CreatedAt:       task.CreatedAt.Time,
+		})
+	}
+
+	return &output, nil
+}
+
+func (s *taskService) ListTasks(ctx context.Context, userId int32) (*[]domain.TaskOutput, error) {
+	var output []domain.TaskOutput
+
+	tasks, err := s.repo.ListTasks(ctx, userId)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, task := range tasks {
+		output = append(output, domain.TaskOutput{
+			ID:              task.ID,
+			UserID:          task.UserID,
+			GoalID:          task.GoalID,
+			Title:           task.Title,
+			IsDone:          task.IsDone,
+			ScheduledDate:   task.ScheduledDate.Time,
+			ScheduledTime:   convertMcSecToTime(task.ScheduledTime.Microseconds),
+			RescheduleCount: task.RescheduleCount,
+			CreatedAt:       task.CreatedAt.Time,
+		})
+	}
+
+	return &output, nil
+}
+
+func (s *taskService) GetTaskByID(ctx context.Context, id int64, userId int32) (*domain.TaskOutput, error) {
+	task, err := s.repo.GetTaskByID(ctx, repo.GetTaskByIDParams{
+		ID:     id,
+		UserID: userId,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	outTask := domain.TaskOutput{
+		ID:              task.ID,
+		UserID:          task.UserID,
+		GoalID:          task.GoalID,
+		Title:           task.Title,
+		IsDone:          task.IsDone,
+		ScheduledDate:   task.ScheduledDate.Time,
+		ScheduledTime:   convertMcSecToTime(task.ScheduledTime.Microseconds),
+		RescheduleCount: task.RescheduleCount,
+		CreatedAt:       task.CreatedAt.Time,
+	}
+
+	return &outTask, nil
+}
+
+func (s *taskService) CreateTask(ctx context.Context, input domain.CreateTaskInput) (*domain.TaskOutput, error) {
+	task, err := s.repo.CreateTask(ctx, repo.CreateTaskParams{
+		UserID: input.UserID,
+		GoalID: input.GoalID,
+		Title:  input.Title,
+		ScheduledDate: pgtype.Date{
+			Time:  input.ScheduledDate,
+			Valid: true,
+		},
+		ScheduledTime: pgtype.Time{
+			Microseconds: input.ScheduledTime.UnixMicro(),
+			Valid:        true,
+		},
+		DurationMinutes: pgtype.Int4{
+			Int32: int32(input.DurationMinutes),
+		},
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return &domain.TaskOutput{
+		ID:              task.ID,
+		UserID:          task.UserID,
+		GoalID:          task.GoalID,
+		Title:           task.Title,
+		IsDone:          task.IsDone,
+		ScheduledDate:   task.ScheduledDate.Time,
+		ScheduledTime:   convertMcSecToTime(task.ScheduledTime.Microseconds),
+		RescheduleCount: task.RescheduleCount,
+		CreatedAt:       task.CreatedAt.Time,
+	}, nil
+}
+
+func (s *taskService) UpdateTask(ctx context.Context, dbTask domain.TaskOutput, updatingTask domain.UpdateTask) (*domain.UpdateTask, error) {
+	if !dbTask.ScheduledDate.IsZero() {
+		updatingTask.RescheduleCount += 1
+	}
+
+	taskUpdatingParams := repo.UpdateTaskByIDParams{
+		ID:     updatingTask.ID,
+		UserID: updatingTask.UserID,
+		GoalID: updatingTask.GoalID,
+		Title:  updatingTask.Title,
+		IsDone: updatingTask.IsDone,
+		ScheduledDate: pgtype.Date{
+			Time:  updatingTask.ScheduledDate,
+			Valid: true,
+		},
+		ScheduledTime: pgtype.Time{
+			Microseconds: updatingTask.ScheduledTime.UnixMicro(),
+			Valid:        true,
+		},
+		DurationMinutes: pgtype.Int4{
+			Int32: int32(updatingTask.DurationMinutes),
+		},
+	}
+
+	err := s.repo.UpdateTaskByID(ctx, taskUpdatingParams)
+	if err != nil {
+		return nil, err
+	}
+
+	return &domain.UpdateTask{
+		ID:              taskUpdatingParams.ID,
+		UserID:          taskUpdatingParams.UserID,
+		GoalID:          taskUpdatingParams.GoalID,
+		Title:           taskUpdatingParams.Title,
+		IsDone:          taskUpdatingParams.IsDone,
+		ScheduledDate:   taskUpdatingParams.ScheduledDate.Time,
+		ScheduledTime:   convertMcSecToTime(taskUpdatingParams.ScheduledTime.Microseconds),
+		RescheduleCount: taskUpdatingParams.RescheduleCount,
+	}, nil
+}
+
+func (s *taskService) AnalyzeForToday(ctx context.Context, userId int32) (repo.CountCompletedTasksForTodayRow, error) {
+	return s.repo.CountCompletedTasksForToday(ctx, userId)
+}
+
+func (s *taskService) DeleteTaskByID(ctx context.Context, id int64, userId int32) error {
+	return s.repo.DeleteTaskByID(ctx, repo.DeleteTaskByIDParams{
+		ID:     id,
+		UserID: userId,
+	})
+}
+
+func convertMcSecToTime(msec int64) time.Time {
+	return time.Date(1, time.January, 1, 0, 0, 0, 0, time.UTC).Add(time.Duration(msec) * time.Microsecond)
+}
